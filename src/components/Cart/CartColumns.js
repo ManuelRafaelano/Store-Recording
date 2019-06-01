f8d36a071d2ceae7fc893d5166970e77c03cecc3
ManuelRafaelano <manuelrafaelano@gmail.com>
2019-06-01 11:41:37 -0600

    Summary (required)

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function CartColumns() {
   return (
-    <div classname="container-fluid text-center d-none d-lg-block">
-      <div classname="row">
+    <div className="container-fluid text-center d-none d-lg-block">
+      <div className="row">
         <div className="col-10 mx-auto col-lg-2">
           <p className="text-uppercase">products</p>
         </div>
